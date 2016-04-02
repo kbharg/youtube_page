d78b3b77d2c5d0af34a63c7548eefd4d77b4cf58
Howard Jiang <howard.yunghao.jiang@gmail.com>
2016-04-01 22:32:02 -0700

    added jquery functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 function postComment() {
 	$('.post').on('click', function() {
-		console.log('post');
-	};
+		if (checkTextArea()) {
+			$('#comments').append(makeComment());
+		}
+	})
+}
+function makeComment() {
+	return '<div class="comment"><div class="image"><img src="img/me.jpg" alt=""></div><div class="commentRight"><div class="nameRow"><a href="#">OscarMike369</a><span>Now</span></div><div class="content">'+document.getElementById('postComment').value+'</div><div class="replyRow"><span class="replyButton">Reply</span><div class="i"><i class="fa fa-circle"></i></div><span class="likeCount">0</span><span class="like likedislike"><i class="fa fa-thumbs-up"></i></span><span class="dislike likedislike"><i class="fa fa-thumbs-down"></i></span></div></div></div>';
+}
+function checkTextArea() {
+	if (document.getElementById('postComment').value) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 $(document).ready(function() {
@@ -32,4 +44,6 @@ $(document).ready(function() {
 		$(this).addClass('dislikedVideo');
 		$(this).siblings().removeClass('likedVideo');
 	})
+
+	postComment();
 })
